5b23ad76cf27e05d5ed5febc42e604eb981955da
“drew” <brandonc503@gmail.com>
2023-02-02 06:55:01 -0500

    Close on click off

--- src/components/pages/AddSale/AutoComplete/index.js
+++ src/components/pages/AddSale/AutoComplete/index.js
@@ -1,14 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getAllItems } from "../../../../controllers/items";
 
 const AutoCompleteItemName = ({ stateAccess }) => {
     const [display, setDisplay] = useState(false)
     const [options, setOptions] = useState([])
     const [search, setSearch] = useState("")
+    const wrapperRef = useRef(null);
 
     useEffect(() => {
         getAllItems().then((data) => setOptions(data));
     }, []);
+
+    useEffect(() => {
+        window.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            window.removeEventListener("mousedown", handleClickOutside);
+        };
+    });
+    
+    const handleClickOutside = event => {
+        const { current: wrap } = wrapperRef;
+        if (wrap && !wrap.contains(event.target)) {
+            setDisplay(false);
+        }
+    };
 
     useEffect(() => {
         parentStates(search)
@@ -30,7 +45,7 @@ const AutoCompleteItemName = ({ stateAccess }) => {
 
     return (
         <>
-            <div >
+            <div ref={wrapperRef}>
                 <input
                     id="item-name"
                     name="itemName"
